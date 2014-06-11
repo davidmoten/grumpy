21219ed54786310d5b5f6f568c891c3b78b5733f
Dave Moten <davidmoten@gmail.com>
2014-06-11 17:16:36 +1000

    update version and docs

--- wms-demo/src/main/java/com/github/davidmoten/geo/wms/demo/CustomLayer.java
+++ wms-demo/src/main/java/com/github/davidmoten/geo/wms/demo/CustomLayer.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import com.github.davidmoten.geo.Position;
 import com.github.davidmoten.geo.projection.Projector;
 import com.github.davidmoten.geo.wms.Layer;
@@ -17,6 +19,8 @@ import com.github.davidmoten.geo.wms.WmsRequest;
 import com.github.davidmoten.geo.wms.WmsUtil;
 
 public class CustomLayer implements Layer {
+
+    private static Logger log = Logger.getLogger(CustomLayer.class);
 
     private static final String CANBERRA = "Canberra";
     private static final double CANBERRA_LAT = -35.3075;
@@ -49,6 +53,7 @@ public class CustomLayer implements Layer {
         Point p = projector.toPoint(CANBERRA_LAT, CANBERRA_LON);
         g.setColor(Color.RED);
         g.setFont(g.getFont().deriveFont(24.0f).deriveFont(Font.BOLD));
+        log.info("point at " + p);
         g.drawString(CANBERRA, p.x + 5, p.y);
 
     }
