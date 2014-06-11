15b78cc6f723502fffe789eea19e412c2e64b0b3
Dave Moten <davidmoten@gmail.com>
2014-06-12 09:15:34 +1000

    simplify

--- grumpy-ogc/src/main/java/com/github/davidmoten/grumpy/wms/Layer.java
+++ grumpy-ogc/src/main/java/com/github/davidmoten/grumpy/wms/Layer.java
@@ -6,20 +6,27 @@ import java.util.Date;
 
 public interface Layer {
 
-    /**
-     * Render some information onto the supplied graphics context
-     * 
-     * @param g
-     *            - the graphics context used for rendering
-     * @param bounds
-     *            - the geo-spatial bounding box of the region to be rendered
-     * @param width
-     *            - of the graphics area in pixels
-     * @param height
-     *            - of the graphics area in pixels
-     */
-    public void render(Graphics2D g, WmsRequest request);
+	/**
+	 * Render some information onto the supplied graphics context
+	 * 
+	 * @param g
+	 *            - the graphics context used for rendering
+	 * @param bounds
+	 *            - the geo-spatial bounding box of the region to be rendered
+	 * @param width
+	 *            - of the graphics area in pixels
+	 * @param height
+	 *            - of the graphics area in pixels
+	 */
+	public void render(Graphics2D g, WmsRequest request);
 
-    String getInfo(Date time, WmsRequest request, Point point, InfoFormat format);
+	/**
+	 * @param time
+	 * @param request
+	 * @param point
+	 * @param mimeType
+	 * @return
+	 */
+	String getInfo(Date time, WmsRequest request, Point point, String mimeType);
 
 }
--- grumpy-ogc/src/main/java/com/github/davidmoten/grumpy/wms/WmsGetCapabilitiesProvider.java
+++ grumpy-ogc/src/main/java/com/github/davidmoten/grumpy/wms/WmsGetCapabilitiesProvider.java
@@ -3,7 +3,5 @@ package com.github.davidmoten.grumpy.wms;
 import javax.servlet.http.HttpServletRequest;
 
 public interface WmsGetCapabilitiesProvider {
-    // doesn't do much but it is useful to have an explicit class for this for
-    // specification to guice injector module
-    String getCapabilities(HttpServletRequest request);
+	String getCapabilities(HttpServletRequest request);
 }
