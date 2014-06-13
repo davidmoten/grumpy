1ab4d0c2bc6211a3a5072158bbddd86738379727
Dave Moten <davidmoten@gmail.com>
2014-06-13 15:53:38 +1000

    add javaodc

--- grumpy-ogc/src/main/java/com/github/davidmoten/grumpy/wms/WmsGetCapabilitiesProvider.java
+++ grumpy-ogc/src/main/java/com/github/davidmoten/grumpy/wms/WmsGetCapabilitiesProvider.java
@@ -2,6 +2,9 @@ package com.github.davidmoten.grumpy.wms;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * Provides the response to the WMS GetCapabilities request.
+ */
 public interface WmsGetCapabilitiesProvider {
 	String getCapabilities(HttpServletRequest request);
 }
