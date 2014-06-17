acbec427867da7a9e2264e4b7df46ef1ce110855
Dave Moten <davidmoten@gmail.com>
2014-06-18 07:13:57 +1000

    add javadoc

--- grumpy-ogc/src/main/java/com/github/davidmoten/grumpy/wms/ImageWriterDefault.java
+++ grumpy-ogc/src/main/java/com/github/davidmoten/grumpy/wms/ImageWriterDefault.java
@@ -7,13 +7,15 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 /**
- * Writes images using {@link ImageIO}.
+ * Writes images using {@link ImageIO}. This can be a bit slow. Significant
+ * performance gains have been obtained using <a
+ * href="http://objectplanet.com/pngencoder/">PNGEncoder</a>). An implementation
+ * has not been included with grumpy because the library is not open source.
  */
 public class ImageWriterDefault implements ImageWriter {
 
     @Override
-    public void writeImage(BufferedImage image, ByteArrayOutputStream os, String imageType)
-            throws IOException {
+    public void writeImage(BufferedImage image, ByteArrayOutputStream os, String imageType) throws IOException {
         ImageIO.write(image, imageType, os);
     }
 
