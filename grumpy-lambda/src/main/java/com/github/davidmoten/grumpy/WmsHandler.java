4afd8e71a542c1c9383961a70d946223abe82b3a
David Moten <davidmoten@gmail.com>
2019-06-04 09:53:30 +1000

    cloudformation dev

--- grumpy-lambda/src/main/java/com/github/davidmoten/grumpy/WmsHandler.java
+++ grumpy-lambda/src/main/java/com/github/davidmoten/grumpy/WmsHandler.java
@@ -3,12 +3,12 @@ package com.github.davidmoten.grumpy;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.Base64;
 import java.util.Map;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.davidmoten.aws.helper.StandardRequestBodyPassThrough;
 
 public class WmsHandler {
@@ -19,25 +19,41 @@ public class WmsHandler {
 
         log.log("starting");
 
-        // expects full request body passthrough from api gateway integration
-        // request
-        StandardRequestBodyPassThrough request = StandardRequestBodyPassThrough.from(input);
-        try (InputStream is = WmsHandler.class.getResourceAsStream("/cloud.png")) {
-            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-            int n;
-            byte[] data = new byte[1024];
-            while ((n = is.read(data, 0, data.length)) != -1) {
-                bytes.write(data, 0, n);
-            }
+        try {
+            // expects full request body passthrough from api gateway integration
+            // request
+            StandardRequestBodyPassThrough request = StandardRequestBodyPassThrough.from(input);
+            try (InputStream is = WmsHandler.class.getResourceAsStream("/cloud.png")) {
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                int n;
+                byte[] data = new byte[1024];
+                while ((n = is.read(data, 0, data.length)) != -1) {
+                    bytes.write(data, 0, n);
+                }
 
-            bytes.flush();
-            String base64 = Base64.getEncoder().encodeToString(bytes.toByteArray());
-//            String response = "{\"statusCode\": 200, " //
-//                    + "\"headers\":{\"Content-Type\": \"image/png\"}, " //
-//                    + "\"body\": \"" + base64 + "\", " //
-//                    + " \"isBase64Encoded\": true}";
-//            log.log("response=\n"+ response);
-            return base64;
+                bytes.flush();
+                String base64 = Base64.getEncoder().encodeToString(bytes.toByteArray());
+                // String response = "{\"statusCode\": 200, " //
+                // + "\"headers\":{\"Content-Type\": \"image/png\"}, " //
+                // + "\"body\": \"" + base64 + "\", " //
+                // + " \"isBase64Encoded\": true}";
+                // log.log("response=\n"+ response);
+                return base64;
+            }
+        } catch (Throwable t) {
+            log.log("error is " + t.getMessage());
+            ByteArrayOutputStream b = new ByteArrayOutputStream();
+            try (PrintStream p = new PrintStream(b)) {
+                t.printStackTrace(p);
+            }
+            log.log("stack trace:\n"+ new String(b.toByteArray()));
+            if (t instanceof RuntimeException) {
+                throw (RuntimeException) t;
+            } else if (t instanceof IOException) {
+                throw (IOException) t;
+            } else {
+                throw new RuntimeException(t);
+            }
         }
     }
 
